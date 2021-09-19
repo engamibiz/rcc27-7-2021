8e1315a731ff7fa0add332d83e84cd7246ed5043
engamibiz <77250446+engamibiz@users.noreply.github.com>
2021-09-19 18:51:37 +0200

    php final

--- php.php
+++ php.php
@@ -1,2 +1,15 @@
 <?php
 echo 'hello world';
+$name='Ahmed';
+$integer = 1;
+$float=1.5;
+$boolean=true;
+
+
+if( $integer > 0 ){
+    echo 'Positive';
+}else if ($integer == 0){
+    echo 'zero';
+}else{
+    echo 'Negative';
+}
